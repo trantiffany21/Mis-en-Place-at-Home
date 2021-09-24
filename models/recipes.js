d67c2bfc3f9efffa63bce90b9d1d0e2f5a65d173
trantiffany21 <tran.tiffany94@gmail.com>
2021-09-24 13:11:33 -0500

    Added recipe schema and updated recipeController

--- models/recipes.js
+++ models/recipes.js
@@ -1,12 +1,31 @@
 const mongoose = require('mongoose')
 
+//subdocument of ingredient list
+const ingredientsSchema = new mongoose.Schema({
+    ingredient: String, 
+    measurement: Number,
+    measurementType: String,
+})
+
+//subdocument of directions
+const directionsSchema = new mongoose.Schema({
+    step: Number, 
+    direction: String
+})
+
 const recipeSchema = new mongoose.Schema({
-    username: {
+    name: {
         type: String,
         required: true,
         unique: true
     },
-    password: String,
+    cuisine: [{type: String}],
+    servingSize: {type: Number},
+    ingredientList: [ingredientsSchema], 
+    urlLink: {type: String}, 
+    youtubeLink: {type: String},
+    directions: [directionsSchema],
+    notes: {type: String},
 })
 
 const Recipe = mongoose.model('Recipe', recipeSchema)
